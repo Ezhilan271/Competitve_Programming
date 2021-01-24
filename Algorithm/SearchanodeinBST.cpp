a23022ebbe1cc680ccce1836537f5c2453fc1bea
Ezhilan G <56919161+Ezhilan271@users.noreply.github.com>
2021-01-24 20:53:49 +0530

    SearchanodeinBST.cpp

---
+++ Algorithm/SearchanodeinBST.cpp
@@ -0,0 +1,8 @@
+bool search(Node* root, int x)
+{
+    //Your code here
+    if(root==nullptr)  return false;
+    if(root->data==x)  return true;
+    if(root->data>=x)  return search(root->left,x);
+    return search(root->right,x);
+}
